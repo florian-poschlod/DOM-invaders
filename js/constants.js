4921d539aba3a5b2e4f396c5d9aabb80e6da95ee
Florian Poschlod <florian.poschlod@mailbox.org>
2021-01-28 11:43:51 +0100

    invader class first iteration done, invader movement adjustet to work with more invaders

---
+++ js/constants.js
@@ -0,0 +1,17 @@
+const INVADER_NAMES = [
+  'tyrant',
+  'vaporizer',
+  'slaughterer',
+  'desolator',
+  'silencer',
+  'devourer',
+  'brain-eater',
+  'suffocator'
+];
+
+const INVADER_BMP = [
+  '../assets/invader-green.png',
+  '../assets/invader-yellow.png',
+  '../assets/invader-pink.png',
+  '../assets/invader-blue.png',
+];
